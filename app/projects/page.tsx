4075e9bde5fc5113a2a7b63e4c9547ad6a795867
Zid <rashidvisda@gmail.com>
2025-10-29 19:14:12 +0800

    fixed vercel deployment

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -19,7 +19,7 @@ export default async function Page() {
     <div className="custom-container">
       <h1 className="text-2xl font-semibold">Projects</h1>
       <h3 className="text-neutral-500 mt-2">
-        A showcase of both private and open-source projects I've built or
+        A showcase of both private and open-source projects I&apos;ve built or
         contributed to.
       </h3>
       <ProjectsGrid initialRepos={repos} />;
