8f39c4aa7a52a8b419b03bafddc8f1ea8c48f171
Zid <rashidvisda@gmail.com>
2025-11-12 18:41:37 +0800

    adjusted padding

--- components/ui/Featured.tsx
+++ components/ui/Featured.tsx
@@ -34,7 +34,7 @@ export default function Featured({ repos }: { repos: Repo[] }) {
           className="
             grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4
             auto-rows-[minmax(180px,auto)]
-            gap-4 xl:gap-2  
+            gap-4 xl:gap-4   mt-6
           "
         >
           {/* ITEM 1 */}
